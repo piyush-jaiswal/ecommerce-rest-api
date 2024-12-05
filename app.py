6eb53e106d3a3fc6211d9fd539c1df830505432a
piyushj <piyushjf@gmail.com>
2024-12-05 20:51:46 +0530

    Introduce .env

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 import routes
