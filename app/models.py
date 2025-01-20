ec45e7e81508706b1df6bd75b024fd35153e8de6
piyush-jaiswal <piyushjf@gmail.com>
2025-01-21 02:42:27 +0530

    add cascading updates and deletes, primary keys, indexes, not nulls

--- app/models.py
+++ app/models.py
@@ -1,23 +1,28 @@
 from datetime import datetime
 
+from sqlalchemy import Index
+
 from app import db
 
 
+# https://stackoverflow.com/questions/2190272/sql-many-to-many-table-primary-key
 category_subcategory = db.Table("category_subcategory",
-                                db.Column("category_id", db.Integer, db.ForeignKey("category.id")),
-                                db.Column("subcategory_id", db.Integer, db.ForeignKey("subcategory.id")))
+                                db.Column("category_id", db.Integer, db.ForeignKey("category.id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True),
+                                db.Column("subcategory_id", db.Integer, db.ForeignKey("subcategory.id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True),
+                                Index("category_subcategory_subcategory_id_idx", "subcategory_id", "category_id"))
 
 subcategory_product = db.Table("subcategory_product",
-                                db.Column("subcategory_id", db.Integer, db.ForeignKey("subcategory.id")),
-                                db.Column("product_id", db.Integer, db.ForeignKey("product.id")))
+                                db.Column("subcategory_id", db.Integer, db.ForeignKey("subcategory.id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True),
+                                db.Column("product_id", db.Integer, db.ForeignKey("product.id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True),
+                                Index("subcategory_product_product_id_idx", "product_id", "subcategory_id"))
 
 
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False, unique=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    subcategories = db.relationship("Subcategory", secondary=category_subcategory, back_populates="categories", lazy='dynamic')
+    name = db.Column(db.String(200), nullable=False, unique=True)   # unique automatically creates a unique index
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    subcategories = db.relationship("Subcategory", secondary=category_subcategory, back_populates="categories", lazy='dynamic', passive_deletes=True)
 
     def to_json(self):
         return {
@@ -32,9 +37,9 @@ class Subcategory(db.Model):
     __tablename__ = 'subcategory'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False, unique=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    categories = db.relationship("Category", secondary=category_subcategory, back_populates="subcategories", lazy='dynamic')
-    products = db.relationship("Product", secondary=subcategory_product, back_populates="subcategories", lazy='dynamic')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    categories = db.relationship("Category", secondary=category_subcategory, back_populates="subcategories", lazy='dynamic', passive_deletes=True)
+    products = db.relationship("Product", secondary=subcategory_product, back_populates="subcategories", lazy='dynamic', passive_deletes=True)
 
     def to_json(self):
         return {
@@ -51,8 +56,8 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False, unique=True)
     description = db.Column(db.String(500))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    subcategories = db.relationship("Subcategory", secondary=subcategory_product, back_populates="products", lazy='dynamic')
+    created_at = db.Column(db.DateTime, nullable=False ,default=datetime.utcnow)
+    subcategories = db.relationship("Subcategory", secondary=subcategory_product, back_populates="products", lazy='dynamic', passive_deletes=True)
 
     def to_json(self):
         return {
