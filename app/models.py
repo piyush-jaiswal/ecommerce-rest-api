85933580694dc6cb12603fb56ac10b334f156df6
piyush-jaiswal <piyushjf@gmail.com>
2025-02-13 20:49:08 +0530

    Add user model, normalize email and hash password

--- app/models.py
+++ app/models.py
@@ -1,8 +1,52 @@
 from datetime import datetime
 
 from sqlalchemy import Index
+from werkzeug.security import generate_password_hash, check_password_hash
+from email_validator import validate_email, EmailNotValidError
+from email_normalize import normalize
 
 from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    email_normalized = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # Does not check for non-deliverable mails. Use check_deliverability or resolve for that which does DNS checks
+    # For more stricter validation, use confirmation emails, or a third party API
+    @staticmethod
+    def _normalize_email(email):
+        # Follows RFCs, allows aliases and only lowers the domain part
+        validated = validate_email(email, check_deliverability=False)
+        # Lowers the local part and normalizes, removes aliases for popular email providers (gmail, yahoo etc)
+        normalized = normalize(validated.email, resolve=False)
+        return normalized
+    
+    @staticmethod
+    def get(email):
+        try:
+            email_normalized = User._normalize_email(email)
+        except EmailNotValidError:
+            return None
+        return User.query.filter_by(email_normalized=email_normalized).scalar()
+    
+    def set_email(self, email):
+        try:
+            self.email_normalized = self._normalize_email(email)
+            self.email = email
+        except EmailNotValidError as e:
+            raise e
+
+    def set_password(self, password):
+        # scrypt stores salt with the hash, which it uses to verify the password
+        self.password_hash = generate_password_hash(password, method='scrypt')
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 # https://stackoverflow.com/questions/2190272/sql-many-to-many-table-primary-key
