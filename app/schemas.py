9b7043e98869b674b876b6aebbe04afdd3f0fbc2
piyush-jaiswal <piyushjf@gmail.com>
2025-09-27 13:59:03 +0530

    migrate category endpoints to flask-smorest, switching to class based views, blueprints, and marshmallow schemas

---
+++ app/schemas.py
@@ -0,0 +1,55 @@
+from marshmallow import Schema, ValidationError, fields, pre_load, validates
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, SQLAlchemySchema, auto_field
+
+from app.models import Category, Product, Subcategory
+
+
+class CategoryOut(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+
+
+class CategoriesOut(Schema):
+    categories = fields.List(fields.Nested(CategoryOut))
+
+
+class CategoryIn(SQLAlchemySchema):
+    class Meta:
+        model = Category
+
+    name = auto_field()
+    subcategories = fields.List(fields.Int())
+
+    @pre_load
+    def strip_strings(self, data, **kwargs):
+        if "name" in data:
+            data["name"] = data["name"].strip()
+
+        return data
+
+    @validates("name")
+    def validate_str_min_len(self, value, data_key):
+        if len(value) < 1:
+            raise ValidationError("Cannot be empty")
+
+
+class SubcategoryOut(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Subcategory
+
+
+class SubcategoriesOut(Schema):
+    subcategories = fields.List(fields.Nested(SubcategoryOut))
+
+
+class ProductOut(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+
+class ProductsOut(Schema):
+    products = fields.List(fields.Nested(ProductOut))
+
+
+class PaginationArgs(Schema):
+    page = fields.Int(load_default=1)
