e15800edade973d4e089b7b68683480d5f51a0da
piyush-jaiswal <piyushjf@gmail.com>
2025-10-16 01:00:18 +0530

    typo

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # initialize extenstions
+    # initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
