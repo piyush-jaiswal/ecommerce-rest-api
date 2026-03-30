356d38fcc963008f869f57af8667fa96a5bfcb6d
piyush-jaiswal <piyushjf@gmail.com>
2026-03-30 23:20:58 +0530

    Remove redundant load_env

--- api/vercel_function.py
+++ api/vercel_function.py
@@ -1,9 +1,7 @@
 import os
 
-from dotenv import load_dotenv
-
 from app import create_app
 
-load_dotenv()
+# load_env already called by importing create_app (by config.py)
 env = os.getenv("VERCEL_ENV", default="production")
 app = create_app(env)
